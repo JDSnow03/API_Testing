163735cac50462747734b16e1797b0897dd715cf
JDSnow03 <jds0100@uah.edu>
2025-04-16 17:06:47 -0500

    finalized vue app test 1

--- my-vue-app/vue.config.js
+++ my-vue-app/vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  devServer: {
-    port: 8080,
-    open: true,
-  },
-  configureWebpack: {
-    // Additional webpack configuration can go here
-  },
-  // Other Vue CLI configurations can be added as needed
+  publicPath: '/API_Testing/'
+  // devServer: {
+  //   port: 8080,
+  //   open: true,
+  // },
+  // configureWebpack: {
+  //   // Additional webpack configuration can go here
+  // },
+  // // Other Vue CLI configurations can be added as needed
+  // vue.config.js
 };
